094631b05f443383f869a6a359d9d6ef3dd4c1b8
Himanshu Yadav <imhimanshu1605@gmail.com>
2025-03-18 17:52:02 +0530

    Update taskRoutes.js

--- task-management-api/routes/taskRoutes.js
+++ task-management-api/routes/taskRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { createTask, getTasks } = require("../controllers/taskController");
+const { createTask, getTasks, updateTask } = require("../controllers/taskController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/tasks", authMiddleware, createTask);
 router.get("/tasks", authMiddleware, getTasks);
-
+router.put("/tasks/:taskId", authMiddleware, updateTask);
 module.exports = router;
